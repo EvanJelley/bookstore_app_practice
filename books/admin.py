6a0fdc9e0b238926e0c8980ef97c8556a4fa26d6
Evan Jelley <jelleyevan@gmail.com>
2024-05-22 12:59:01 -0600

    Ch.12 build reviews model

--- books/admin.py
+++ books/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 
-from .models import Books
+from .models import Books, Reviews
+
+class ReviewsInline(admin.TabularInline):
+    model = Reviews
 
 class BooksAdmin(admin.ModelAdmin):
+    inlines = [
+        ReviewsInline,
+    ]
     list_display = ('title', 'author', 'price')
 
 admin.site.register(Books, BooksAdmin)
